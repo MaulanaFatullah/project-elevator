9786f99f4a5c08afdc9446ffe76c542db5ec6d1a
Maulana Fatullah <fistalanid@gmail.com>
2022-11-30 18:32:03 +0700

    Project Elevator v0.1

---
+++ public/js/tre-app.js
@@ -0,0 +1,26 @@
+//Navbar Fixed
+window.onscroll = function () {
+    const header = document.querySelector('header');
+    const treIcon = document.querySelector('#tre-icon');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+        // navMenu.classList.add('nav-padding');
+        treIcon.classList.add('tre-icon');
+    } else {
+        header.classList.remove('navbar-fixed');
+        // navMenu.classList.remove('nav-padding');
+        treIcon.classList.remove('tre-icon');
+
+    }
+};
+
+// Nav Menu
+const hamburgerMenu = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburgerMenu.addEventListener('click', function () {
+    hamburgerMenu.classList.toggle('hamburger-active');
+    navMenu.classList.toggle('nav-active');
+});
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0C2F51',
+        secondary: '#F2E5E5',
+        accent: '#FF0000',
+        dark: '#2B3A55',
+        'base-color': '#FEFCF3',
+      }
+    },
+  },
+  plugins: [],
+}
